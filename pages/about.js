6d4e5c28e760d17f6f849ce1f426314003fd0070
kiprotichdominic <kiprotichdominic102@gmail.com>
2023-03-17 22:31:57 +0300

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, sit.
+      Voluptate nesciunt illo vero veritatis et quidem maiores veniam
+      accusantium recusandae reprehenderit ipsam nihil hic soluta sunt doloribus
+      voluptates, explicabo maxime temporibus a labore quae architecto velit aut
+      obcaecati! Quos incidunt, fugiat suscipit consequatur commodi praesentium
+      aspernatur laboriosam eaque eligendi at dolor odio obcaecati, sint
+      explicabo, omnis modi ratione quo harum tempora eveniet repellendus
+      cupiditate. Quisquam, rem at, architecto enim optio eaque omnis dolor
+      voluptatibus iure nobis laudantium distinctio, hic reiciendis officia!
+      Placeat vitae enim recusandae provident, sint cum asperiores labore nam
+      magni perferendis veritatis! Aut voluptatibus dolorum nobis tempore.
+    </div>
+  );
+}
+
+export default About;
