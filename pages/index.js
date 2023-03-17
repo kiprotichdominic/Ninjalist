49a79bf548b6f1c07436c9e5163277d7fd7f6847
kiprotichdominic <kiprotichdominic102@gmail.com>
2023-03-17 23:33:41 +0300

    add module css

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,21 @@
 import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
-      <h1>Home Page</h1>
-      <p>
+      <h1 className={styles.title}>Home Page</h1>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium
         nihil, labore sed non ea nostrum deleniti numquam tempora quae
         voluptatibus aliquid praesentium molestiae quasi reiciendis est optio.
-        Non quo temporibus, saepe recusandae explicabo deleniti nobis nisi
-        minima dolorem cumque cupiditate enim, consequatur modi sint. Molestias
-        atque inventore sed harum eos, ut reprehenderit, voluptas aperiam
-        sapiente odit quidem nesciunt laudantium unde. Harum, sunt amet quidem
-        veniam et quia cumque voluptates maiores, dicta dolorum voluptatum
-        quibusdam nostrum veritatis explicabo magnam assumenda molestiae magni
-        modi aliquam delectus! Officiis modi neque, aut, possimus alias totam
-        eligendi nulla similique blanditiis dolorum dignissimos laudantium,
-        magnam obcaecati?
       </p>
-      <Link href="/ninjas">See Ninja Listing</Link>
+      <p className={styles.text}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium
+        nihil, labore sed non ea nostrum deleniti numquam tempora quae
+        voluptatibus aliquid praesentium molestiae quasi reiciendis est optio.
+      </p>
+      <Link href="/ninjas" className={styles.btn}>See Ninja Listing</Link>
     </>
   );
 }
