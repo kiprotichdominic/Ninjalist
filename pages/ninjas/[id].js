fb43887fb82e31068a229ceb0d8328eb94d8b3fd
kiprotichdominic <kiprotichdominic102@gmail.com>
2023-03-18 11:21:18 +0300

    show detail page

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,11 +1,37 @@
-import React from 'react'
+export const getStaticPaths = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  const paths = data.map((ninja) => {
+    return {
+      params: { id: ninja.id.toString() },
+    };
+  });
+  return { paths, fallback: false };
+};
 
-function NinjaDetails() {
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  );
+  const data = await response.json();
+  return {
+    props: {
+      ninja: data,
+    },
+  };
+};
+
+function NinjaDetails({ninja}) {
   return (
     <div>
-        <h1>Details Page</h1>
+      <h1>{ninja.name}</h1>
+      <p>{ninja.email}</p>
+      <p>{ninja.website}</p>
+      <p>{ninja.address.city}</p>
+
     </div>
-  )
+  );
 }
 
-export default NinjaDetails
+export default NinjaDetails;
